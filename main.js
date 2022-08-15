fa27434d7ef47ec36849f15c0140cf36b47b516a
Megan <meganmoulos@gmail.com>
2022-08-14 20:21:04 -0700

    commit tree 70f381a832c361090b791474cd48dfa7dee707ab

--- main.js
+++ main.js
@@ -4,6 +4,44 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// When user clicks an empty heart
+// Invoke mimicServerCall to simulate making a server request
+
+const likeHeartArr = document.querySelectorAll('.like-glyph')
+
+likeHeartArr.forEach(e => {
+  e.addEventListener('click', () => {
+
+    // When a user clicks on a full heart change the heart back to an empty heart and remove the activated-heart class
+    if (e.style.color === 'red') {
+      e.classList.remove('activated-heart')
+      e.textContent = EMPTY_HEART
+      e.style.color = ''
+      return
+    }
+    let promiseResult = mimicServerCall(e)
+
+    // When the server returns a failure status respond to the error with a catch block after the then block
+    // When the server returns a success status change the heart to a full heart
+    promiseResult.then(() => {
+      console.log('success')
+      e.classList.add('activated-heart')
+      e.style.color = 'red'
+      e.textContent = FULL_HEART
+    })
+
+    // Display the error modal and modal message if rejected
+    .catch(() => {
+      document.querySelector('#modal').classList.remove('hidden')
+      document.getElementById('modal-message').textContent = 'Random server error. Try again.'
+      
+    // SetTimeout for error modal to 3 seconds
+    setTimeout(function() {
+      document.querySelector('#modal').classList.add('hidden')
+    }, 3000)
+    })
+  })
+})
 
 
 
@@ -23,3 +61,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
